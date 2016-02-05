87259c8ac83f3d5c08dcc691c8b41bdb3561c297
Martín <mpugatelecouvigo@gmail.com>
2016-02-05 18:26:51 +0100

    Intro_Sockets V0.0.0

---
+++ Intro_Sockets/src/Intro_Sockets.java
@@ -0,0 +1,24 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Intro_Sockets {
+	public static void main (String args[]){
+		String aplicacion=args[0].toLowerCase();
+		switch (aplicacion) {
+		case "s": //SERVIDOR
+			Integer puerto = Integer.parseInt(args[1]);
+			new Servidor(puerto);
+			break;
+		case "c": //CLIENTE
+			InetAddress IPServidor;
+			Integer puertoServer = Integer.parseInt(args[2]);
+			try {
+				IPServidor = InetAddress.getByName(args[1]);
+				new Cliente(IPServidor, puertoServer);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
+	}
+}
